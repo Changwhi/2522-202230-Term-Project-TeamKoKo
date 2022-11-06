92a6ee62f54de60c7ed21a7a4e13007339b057d6
Changwhi <whinada@gmail.com>
2022-11-06 14:13:13 -0800

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.teamkoko {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.bcit.comp2522.termproject.teamkoko to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.teamkoko;
+}
