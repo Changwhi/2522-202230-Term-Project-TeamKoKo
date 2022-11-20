e8367f14880e19d5b78c9b4772b4f846b2f94cd1
Changwhi <whinada@gmail.com>
2022-11-20 15:10:48 -0800

    Add java doc

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/Enemy.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/Enemy.java
@@ -1,5 +1,10 @@
 package ca.bcit.comp2522.termproject.teamkoko;
 
+/**
+ * Enemy class.
+ * @author Jesper
+ * @version 1.0.0
+ */
 public class Enemy {
     private int pointX;
     private int pointY;
@@ -9,6 +14,9 @@ public class Enemy {
     private Boolean singASong;
     private int readingSpeed;
 
+    /**
+     * A default constructor.
+     */
     public Enemy() {
         this.pointX = 310;
         this.pointY = 0;
@@ -19,59 +27,110 @@ public class Enemy {
         this.readingSpeed = 1;
     }
 
+    /**
+     * Returns X-coordinate.
+     * @return Integer
+     */
     public int getPointX() {
         return pointX;
     }
-
+    /**
+     * Returns Y-coordinate.
+     * @return Integer
+     */
     public int getPointY() {
         return pointY;
     }
 
-    public void setPointX(int pointX) {
+    /**
+     * Set pointX to new position.
+     * @param pointX Integer
+     */
+    public void setPointX(final int pointX) {
         this.pointX = pointX;
     }
 
-    public void setPointY(int pointY) {
+    /**
+     * Set pointY to new position.
+     * @param pointY Integer
+     */
+    public void setPointY(final int pointY) {
         this.pointY = pointY;
     }
 
+    /**
+     * Returns the name of the phases.
+     * @return String
+     */
     public String getLevel1() {
         return level1;
     }
-
+    /**
+     * Returns the name of the phases.
+     * @return String
+     */
     public String getLevel2() {
         return level2;
     }
-
+    /**
+     * Returns the name of the phases.
+     * @return String
+     */
     public String getLevel3() {
         return level3;
     }
 
-    public void setLevel1(String level1) {
+    /**
+     * Set the new name of the phases.
+     * @param level1 String
+     */
+    public void setLevel1(final String level1) {
         this.level1 = level1;
     }
-
-    public void setLevel2(String level2) {
+    /**
+     * Set the new name of the phases.
+     * @param level2 String
+     */
+    public void setLevel2(final String level2) {
         this.level2 = level2;
     }
-
-    public void setLevel3(String level3) {
+    /**
+     * Set the new name of the phases.
+     * @param level3 String
+     */
+    public void setLevel3(final String level3) {
         this.level3 = level3;
     }
 
+    /**
+     * Returns True, if song is playing during the game.
+     * @return Boolean
+     */
     public Boolean getSingASong() {
         return singASong;
     }
 
-    public void setSingASong(Boolean singASong) {
+    /**
+     * Sets singASong to True, if music is playing. Otherwise, sets a variable False.
+     * @param singASong Boolean
+     */
+    public void setSingASong(final Boolean singASong) {
         this.singASong = singASong;
     }
 
+    /**
+     * Returns the playing speed of the music.
+     * @return Integer
+     */
     public int getReadingSpeed() {
         return readingSpeed;
     }
 
-    public void setReadingSpeed(int readingSpeed) {
+    /**
+     * Set a readingSpeed to a new playing speed of the music.
+     * @param readingSpeed Integer
+     */
+    public void setReadingSpeed(final int readingSpeed) {
         this.readingSpeed = readingSpeed;
     }
 
--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/Player.java
@@ -1,47 +1,93 @@
 package ca.bcit.comp2522.termproject.teamkoko;
 
-abstract public class Player {
+/**
+ * Abstract class that defines Characters.
+ * @author Jesper and Changwhi Oh
+ * @version 1.0.0
+ */
+public abstract class Player {
 
     private int pointX;
     private int pointY;
     private boolean arrive;
     private boolean win;
 
-    public Player () {
+    /**
+     * A default constructor.
+     */
+    public Player() {
         this.pointX = 0;
         this.pointY = 0;
         this.arrive = false;
         this.win = false;
     }
 
+    /**
+     * moves character to desire location.
+     */
     public void move() {
+        }
 
-    }
-
+    /**
+     * Returns x-coordinate.
+     * @return Integer
+     */
     public int getPointX() {
         return this.pointX;
     }
+
+    /**
+     * Returns Y-coordinate.
+     * @return Integer
+     */
     public int getPointY() {
         return this.pointY;
     }
-    public void setPointX(int newPoint) {
+
+    /**
+     * Set pointX to the new coordinate.
+     * @param newPoint Integer
+     */
+    public void setPointX(final int newPoint) {
         this.pointX = newPoint;
     }
-    public void setPointY(int newPoint) {
+    /**
+     * Set pointY to the new coordinate.
+     * @param newPoint Integer
+     */
+    public void setPointY(final int newPoint) {
         this.pointY = newPoint;
     }
+
+    /**
+     * Returns Ture, if character is in a specific position. Otherwise, returns False.
+     * @return Boolean
+     */
     public boolean isArrive() {
         return arrive;
     }
-    public void setArrive(boolean newArrive) {
+
+    /**
+     * Sets the newArrive to True, if character arrives at a specific position. Otherwise, sets the variable to False.
+     * @param newArrive Boolean
+     */
+    public void setArrive(final boolean newArrive) {
         this.arrive = newArrive;
     }
 
+    /**
+     * Returns True, if player wins. Otherwise, Returns False.
+     * @return Boolean
+     */
     public boolean isWin() {
         return win;
     }
 
-    public void setWin(boolean newWin) {
+    /**
+     * Set newWin to True, If player wins, Otherwise, Set a variable to False.
+     * @param newWin Boolean
+     */
+    public void setWin(final boolean newWin) {
         this.win = newWin;
     }
 
