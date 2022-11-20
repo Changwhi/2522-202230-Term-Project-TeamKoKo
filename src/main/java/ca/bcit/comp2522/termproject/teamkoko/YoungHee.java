a6580b13f68908efe7f090678faccbc91939a79e
Changwhi <whinada@gmail.com>
2022-11-20 13:24:50 -0800

    Add java doc

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/YoungHee.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/YoungHee.java
@@ -1,19 +1,36 @@
 package ca.bcit.comp2522.termproject.teamkoko;
 
-public class YoungHee extends Player{
+/**
+ * One of the character that player can choose.
+ * @author Changwhi Oh
+ * @version 1.0.0
+ */
+public class YoungHee extends Player {
 
     private final String name;
     private final String skill;
 
+    /**
+     * A default constructor.
+     */
     public YoungHee() {
         super();
         this.name = "YoungHee";
         this.skill = "Jump";
     }
 
+    /**
+     * Returns character's name.
+     * @return String
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Returns skill name.
+     * @return String
+     */
     public String getSkill() {
         return this.skill;
     }
