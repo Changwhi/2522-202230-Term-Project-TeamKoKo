3cf804cae1e245fa83c424a0581ac6103fe96a71
Changwhi <whinada@gmail.com>
2022-11-20 13:23:33 -0800

    Add java doc

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/ChulSoo.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/ChulSoo.java
@@ -1,18 +1,35 @@
 package ca.bcit.comp2522.termproject.teamkoko;
 
-public class ChulSoo {
+/**
+ * one of the characters that player can choose.
+ * @author Changwhi Oh
+ * @version 1.0.0
+ */
+public class ChulSoo extends Player {
     private final String name;
     private final String skill;
 
+    /**
+     * A default constructor.
+     */
     public ChulSoo() {
         super();
         this.name = "ChulSoo";
         this.skill = "Teleport";
     }
 
+    /**
+     * Returns Character's name.
+     * @return String
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Returns Skill name.
+     * @return String
+     */
     public String getSkill() {
         return this.skill;
     }
