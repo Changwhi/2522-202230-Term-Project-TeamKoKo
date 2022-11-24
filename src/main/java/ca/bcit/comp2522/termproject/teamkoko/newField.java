640a96ebe18f9afeea255ee35db4a100a5b6f5bf
Changwhi <whinada@gmail.com>
2022-11-24 15:35:50 -0800

    Implement a feature to pop up main window

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/newField.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/newField.java
@@ -1,12 +1,18 @@
 package ca.bcit.comp2522.termproject.teamkoko;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -27,6 +33,9 @@ public class NewField extends Application {
     Senna senna = new Senna(40,50,0,0,ivSenna);
     static Pane root = new Pane();
 
+    //Test
+    Stage window;
+    Scene scene1, scene2;
 
     /**
      * Displays an image that can be moved using the arrow keys.
@@ -35,29 +44,72 @@ public class NewField extends Application {
      */
     public void start(Stage primaryStage) {
 
-        ivSenna.setX(280);
-        ivSenna.setY(736);
+        window = primaryStage;
 
-        root.setPrefSize(640, 800);
-        root.getChildren().addAll(senna);
+        // label1, button1
 
+        Label label1 = new Label("JAVA FX Label");
+        Button button1 = new Button("Start");
+        button1.setOnAction(e->window.setScene(scene2));
 
+        // layout1
 
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 300, 300);
+
+        // button 2
+
+        Button button2 = new Button("Go back to Scene 1");
+        button2.setOnAction(e->window.setScene(scene1));
+
+        // layout 2
 
         ImageView iv = new ImageView("field.png");
-//
         Group root2 = new Group(iv);
+        ivSenna.setX(280);
+        ivSenna.setY(736);
         root2.getChildren().add(senna);
 
+        scene2 = new Scene(root2);
 
-        Scene scene = new Scene(root2);
+        window.setScene(scene1);
+        window.setTitle("Title Here");
+        window.show();
 
-        // Register the key listener here
-        scene.setOnKeyPressed(this::processKeyPress);
 
-        primaryStage.setTitle("AlienDirection");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+
+
+        scene2.setOnKeyPressed(this::processKeyPress);
+
+
+//        //Display map
+//        ivSenna.setX(280);
+//        ivSenna.setY(736);
+//
+//        root.setPrefSize(640, 800);
+//        root.getChildren().addAll(senna);
+//
+//
+//
+//
+//        ImageView iv = new ImageView("field.png");
+////
+//        Group root2 = new Group(iv);
+//        root2.getChildren().add(senna);
+//
+//
+//        Scene scene = new Scene(root2);
+//
+//        // Register the key listener here
+//        scene.setOnKeyPressed(this::processKeyPress);
+//
+//        primaryStage.setTitle("AlienDirection");
+//        primaryStage.setScene(scene);
+//        primaryStage.hide();
+
+
     }
 
 
