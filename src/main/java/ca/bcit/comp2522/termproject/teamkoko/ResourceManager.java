b252760e8fb9b81d22272f5ec7fc433bbe9093fe
seong <seonghyun91@gmail.com>
2022-11-28 20:14:00 -0800

    Implement ResourceManager class

---
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/ResourceManager.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2522.termproject.teamkoko;
+
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+/**
+ * ResourceManager class
+ * @author Jesper
+ * @version 1
+ */
+
+public class ResourceManager {
+
+
+}
