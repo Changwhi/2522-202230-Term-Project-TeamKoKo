203620ac2cbf1f598c40c49a87bf444e51e1bb26
seong <seonghyun91@gmail.com>
2022-11-28 20:45:48 -0800

    Create SaveData class

---
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/SaveData.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2522.termproject.teamkoko;
+
+
+
+
+/**
+ * Class that allows Save / Load feature
+ *
+ * @author Jesper Hong
+ * @version 1.0
+ */
+public class SaveData implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+}
