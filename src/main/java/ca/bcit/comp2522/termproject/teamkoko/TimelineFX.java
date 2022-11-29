827af92a7b7f4c21f6beb0de62d6967c81e1103f
seong <seonghyun91@gmail.com>
2022-11-28 20:40:17 -0800

    Create TimelineFX class

---
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/TimelineFX.java
@@ -0,0 +1,17 @@
+package ca.bcit.comp2522.termproject.teamkoko;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+public class TimelineFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
