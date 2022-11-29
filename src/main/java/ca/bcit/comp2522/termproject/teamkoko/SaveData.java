c42b58896e621d8f2e63a9837835ea7975204d39
seong <seonghyun91@gmail.com>
2022-11-28 20:46:43 -0800

    Add instance variables in SaveData class to store game data to save the game

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/SaveData.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/SaveData.java
@@ -12,5 +12,24 @@ package ca.bcit.comp2522.termproject.teamkoko;
 public class SaveData implements java.io.Serializable {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * X position of character
+     */
+    public double posX;
+
+    /**
+     * Y position of Character.
+     */
+    public double posY;
+
+    /**
+     * Returns whether the character is alive
+     */
+    public boolean isAlive;
+
+    /**
+     * Return game over
+     */
+    public boolean gameOver;
 
 }
