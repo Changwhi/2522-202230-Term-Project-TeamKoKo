991146ce1ad46b66054dbde7c092ea8a9689d025
Changwhi <whinada@gmail.com>
2022-12-02 19:23:10 -0800

    restore because of error

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/SoundClipTest.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/SoundClipTest.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.net.URL;
 
 //extends JFrame
-public class SoundClipTest {
-
-
-
+public class SoundClipTest extends JFrame{
     public SoundClipTest() {
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.setTitle("Test Sound Clip");
+//        this.setSize(300, 200);
+//        this.setVisible(true);
 
     }
 
@@ -25,6 +26,24 @@ public class SoundClipTest {
             clip.open(audioIn);
             clip.start();
 
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    public void stopSong() {
+        try {
+            // Open an audio input stream.
+            URL url = this.getClass().getClassLoader().getResource("gameover.wav");
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+            // Get a sound clip resource.
+            Clip clip = AudioSystem.getClip();
+            // Open audio clip and load samples from the audio input stream.
+            clip.open(audioIn);
+            clip.stop();
 
         } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
@@ -33,10 +52,7 @@ public class SoundClipTest {
         } catch (LineUnavailableException e) {
             e.printStackTrace();
         }
+    }
 
     }
 
-    public void stopSong() {
-//        clip.stop();
-    }
-}
