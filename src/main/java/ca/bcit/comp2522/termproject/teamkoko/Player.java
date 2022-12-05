c6d0931610ff97bdc3c1bb4c87049f99d4ada05d
seong <seonghyun91@gmail.com>
2022-12-04 22:19:25 -0800

    Javadoc for Player

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/Player.java
@@ -4,21 +4,46 @@ import javafx.scene.layout.Pane;
 
 /**
  * Abstract class that defines Characters.
+ *
  * @author Jesper and Changwhi Oh
  * @version 1.0.0
  */
 public abstract class Player extends Pane {
 
+    /**
+     * Character image width.
+     */
     protected int width;
+
+    /**
+     * Character image height.
+     */
     protected int height;
-    int pointX;
-    int pointY;
+
+    /**
+     * Character's status.
+     */
     protected boolean arrive;
+
+    /**
+     * Character's winning status.
+     */
     protected boolean win;
 
     /**
+     * Character's X coordinate.
+     */
+    int pointX;
+    /**
+     * Character's Y coordinate.
+     */
+    int pointY;
+
+
+    /**
      * Constructs a Player object.
-     * @param width width of this character in pixel
+     *
+     * @param width  width of this character in pixel
      * @param height height of this character in pixel
      * @param pointX X coordinate of offset in Imageview(Starts from left top)
      * @param pointY Y coordinate of offset in Imageview(Starts from left top)
@@ -36,10 +61,11 @@ public abstract class Player extends Pane {
      * moves character to desire location.
      */
     public void move() {
-        }
+    }
 
     /**
      * Returns x-coordinate.
+     *
      * @return Integer
      */
     public int getPointX() {
@@ -48,6 +74,7 @@ public abstract class Player extends Pane {
 
     /**
      * Returns Y-coordinate.
+     *
      * @return Integer
      */
     public int getPointY() {
@@ -56,13 +83,16 @@ public abstract class Player extends Pane {
 
     /**
      * Set pointX to the new coordinate.
+     *
      * @param newPoint Integer
      */
     public void setPointX(final int newPoint) {
         this.pointX = newPoint;
     }
+
     /**
      * Set pointY to the new coordinate.
+     *
      * @param newPoint Integer
      */
     public void setPointY(final int newPoint) {
@@ -71,6 +101,7 @@ public abstract class Player extends Pane {
 
     /**
      * Returns Ture, if character is in a specific position. Otherwise, returns False.
+     *
      * @return Boolean
      */
     public boolean isArrive() {
@@ -79,6 +110,7 @@ public abstract class Player extends Pane {
 
     /**
      * Sets the newArrive to True, if character arrives at a specific position. Otherwise, sets the variable to False.
+     *
      * @param newArrive Boolean
      */
     public void setArrive(final boolean newArrive) {
@@ -87,6 +119,7 @@ public abstract class Player extends Pane {
 
     /**
      * Returns True, if player wins. Otherwise, Returns False.
+     *
      * @return Boolean
      */
     public boolean isWin() {
@@ -95,14 +128,12 @@ public abstract class Player extends Pane {
 
     /**
      * Set newWin to True, If player wins, Otherwise, Set a variable to False.
+     *
      * @param newWin Boolean
      */
     public void setWin(final boolean newWin) {
         this.win = newWin;
     }
-
-
-
 
 
 }
