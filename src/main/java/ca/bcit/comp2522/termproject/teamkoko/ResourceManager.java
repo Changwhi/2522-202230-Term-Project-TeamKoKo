8bee76831bc6e01e623257945b7b1bb8291ec13f
seong <seonghyun91@gmail.com>
2022-12-04 22:12:23 -0800

    Javadoc for ResourceManager

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/ResourceManager.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/ResourceManager.java
@@ -9,32 +9,38 @@ import java.nio.file.Paths;
 
 
 /**
- * ResourceManager class
+ * ResourceManager class.
+ *
  * @author Jesper
  * @version 1
  */
 
-public class ResourceManager {
+public final class ResourceManager {
+
+    private ResourceManager() {
+    }
 
     /**
-     * Saves the game
-     * @param data datafile
+     * Saves the game.
+     *
+     * @param data     datafile
      * @param fileName filename of this save_file
      * @throws Exception
      */
-    public static void save(Serializable data, String fileName) throws Exception {
+    public static void save(final Serializable data, final String fileName) throws Exception {
         try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName)))) {
             oos.writeObject(data);
         }
     }
 
     /**
-     * Loads the game
+     * Loads the game.
+     *
      * @param fileName fileName
      * @return savefile as Object
      * @throws Exception
      */
-    public static Object load(String fileName) throws Exception {
+    public static Object load(final String fileName) throws Exception {
         try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(fileName)))) {
             return ois.readObject();
         }
