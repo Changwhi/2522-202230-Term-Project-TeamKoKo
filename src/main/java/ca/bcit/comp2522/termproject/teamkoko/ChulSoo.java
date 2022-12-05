29d9ff5ed8f6512ee60bb6ac12897942393a11a8
seong <seonghyun91@gmail.com>
2022-12-04 21:35:14 -0800

    Comment for Chulsoo class

--- src/main/java/ca/bcit/comp2522/termproject/teamkoko/ChulSoo.java
+++ src/main/java/ca/bcit/comp2522/termproject/teamkoko/ChulSoo.java
@@ -6,22 +6,38 @@ import javafx.scene.image.ImageView;
 
 /**
  * one of the characters that player can choose.
+ *
  * @author Changwhi Oh
  * @version 1.0.0
  */
 public class ChulSoo extends Player {
     private final String name;
     private final String skill;
+
+    /**
+     * ImageView field.
+     */
     ImageView imageView;
 
 
+    /**
+     * Image of Chulsoo character.
+     */
     Image chulSooImage;
 
+    /**
+     * ImageView for ChulSoo character.
+     */
     ImageView ivChulSu;
 
 
     /**
-     * A default constructor.
+     * Constructs Chulsoo object.
+     *
+     * @param width  width as int
+     * @param height height as int
+     * @param pointX x coordinate as int
+     * @param pointY y coordinate as int
      */
     public ChulSoo(final int width, final int height, final int pointX, final int pointY) {
         super(width, height, pointX, pointY);
@@ -38,6 +54,7 @@ public class ChulSoo extends Player {
 
     /**
      * Returns Character's name.
+     *
      * @return String
      */
     public String getName() {
@@ -46,21 +63,34 @@ public class ChulSoo extends Player {
 
     /**
      * Returns Skill name.
+     *
      * @return String
      */
     public String getSkill() {
         return this.skill;
     }
 
-    public void setChulsuX (double setNewPoint) {
+    /**
+     * Setter for ivChulsoo's posX.
+     * @param setNewPoint  X position as double
+     */
+    public void setChulsuX(final double setNewPoint) {
         this.ivChulSu.setX(setNewPoint);
     }
 
 
-    public void setChulsuY (double setNewPoint) {
+    /**
+     * Setter for ivChulsoo's posY.
+     * @param setNewPoint  Y position as double
+     */
+    public void setChulsuY(final double setNewPoint) {
         this.ivChulSu.setY(setNewPoint);
     }
 
+    /**
+     * Gets ivChulsoo.
+     * @return ivChulsoo as ImageView
+     */
     public ImageView getIvChulSu() {
         return ivChulSu;
     }
